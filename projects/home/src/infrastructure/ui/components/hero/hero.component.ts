accad8d11e9f128895146689c032313746873580
Sergio Fernandez <sergioadrianfernandez02@gmail.com>
2025-03-08 10:49:16 -0300

    feat(home): Implement flight search form and hero component UI

--- projects/home/src/infrastructure/ui/components/hero/hero.component.ts
+++ projects/home/src/infrastructure/ui/components/hero/hero.component.ts
@@ -1,9 +1,44 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'lib-hero',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
-export class HeroComponent {}
+export class HeroComponent {
+  flightForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.flightForm = this.fb.group({
+      tripType: ['roundTrip', Validators.required],
+      passengers: [1, Validators.required],
+      useMiles: [false],
+      origin: ['', Validators.required],
+      destination: ['', Validators.required],
+      dates: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.flightForm.valid) {
+      console.log('Search flights with:', this.flightForm.value);
+    }
+  }
+
+  toggleTripType(type: string) {
+    this.flightForm.get('tripType')?.setValue(type);
+  }
+
+  toggleUseMiles(event: Event) {
+    const checkbox = event.target as HTMLInputElement;
+    this.flightForm.get('useMiles')?.setValue(checkbox.checked);
+  }
+}
