4710897eaae4ff732d898d512697d0f6743cea9e
Sebastian barrera <sebasbh2008@hotmail.com>
2025-03-08 11:34:54 -0500

    chore: :construction: Defining the project architecture

---
+++ projects/admin/src/infrastructure/ui/layouts/admin-layout/admin-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+@Component({
+  selector: 'lib-admin-layout',
+  imports: [RouterOutlet],
+  templateUrl: './admin-layout.component.html',
+  styleUrl: './admin-layout.component.scss'
+})
+export class AdminLayoutComponent {
+
+}
---
+++ projects/admin/src/infrastructure/ui/routes/admin.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { AdminLayoutComponent } from "../layouts/admin-layout/admin-layout.component";
+
+export const adminRoutes: Routes = [
+    {
+        path: '',
+        component: AdminLayoutComponent,
+    }
+]
