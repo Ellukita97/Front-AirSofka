8a871318e10f7135be8cc6861a401a3c101ce8bb
Sebastian barrera <sebasbh2008@hotmail.com>
2025-03-08 14:35:38 -0500

    feat: :sparkles: Added sidebar to the navigation panel

--- projects/admin/src/infrastructure/ui/layouts/admin-layout/admin-layout.component.ts
+++ projects/admin/src/infrastructure/ui/layouts/admin-layout/admin-layout.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidenavComponent } from '../../components/sidenav/sidenav.component';
 @Component({
   selector: 'lib-admin-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidenavComponent],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
