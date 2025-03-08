00c2c22c8fda11a99ad85b3e21e13c47d4819aa8
Sergio Fernandez <sergioadrianfernandez02@gmail.com>
2025-03-07 21:18:37 -0300

    feat(shared): Add BodyLayoutComponent, LogoComponent, and INavRouter to public API

---
+++ projects/home/src/infrastructure/ui/components/hero/hero.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-hero',
+  imports: [],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.scss',
+})
+export class HeroComponent {}
