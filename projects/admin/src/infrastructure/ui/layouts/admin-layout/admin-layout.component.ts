5550f8b7e5c30bdd3c67142a19083149869462ca
Sebastian barrera <sebasbh2008@hotmail.com>
2025-03-08 20:59:52 -0500

    feat: :construction: accordion structure

--- projects/admin/src/infrastructure/ui/layouts/admin-layout/admin-layout.component.ts
+++ projects/admin/src/infrastructure/ui/layouts/admin-layout/admin-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from '../../components/sidenav/sidenav.component';
+import { AccordionComponent } from "../../components/accordion/accordion.component";
 @Component({
   selector: 'lib-admin-layout',
-  imports: [RouterOutlet, SidenavComponent],
+  imports: [RouterOutlet, SidenavComponent, AccordionComponent],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
