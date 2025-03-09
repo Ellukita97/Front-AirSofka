10d4574ff6b57365728dd1795d7cd1d6168199f7
Sergio Fernandez <sergioadrianfernandez02@gmail.com>
2025-03-09 10:12:51 -0300

    feat(home): Implement recomendations section and integrate into home

--- projects/home/src/infrastructure/ui/containers/home/home.component.ts
+++ projects/home/src/infrastructure/ui/containers/home/home.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { NewDestinyComponent } from '../../components/new-destiny/new-destiny.component';
 import { BestFlightsComponent } from '../../components/best-flights/best-flights.component';
+import { RecomendationsSectionComponent } from '../../components/recomendations-section/recomendations-section.component';
 
 @Component({
   selector: 'lib-home',
-  imports: [HeroComponent, NewDestinyComponent, BestFlightsComponent],
+  imports: [
+    HeroComponent,
+    NewDestinyComponent,
+    BestFlightsComponent,
+    RecomendationsSectionComponent,
+  ],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
