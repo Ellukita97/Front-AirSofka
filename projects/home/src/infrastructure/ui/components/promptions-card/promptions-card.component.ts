5d35fe0cb28956423607e4a48bce598c43b0320c
Sergio Fernandez <sergioadrianfernandez02@gmail.com>
2025-03-09 12:25:30 -0300

    feat(home): Implement promotions section

--- projects/home/src/infrastructure/ui/components/promptions-card/promptions-card.component.ts
+++ projects/home/src/infrastructure/ui/components/promptions-card/promptions-card.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CardPromotionModel } from '../../../../domain/model/cardPromotion.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'lib-promptions-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './promptions-card.component.html',
   styleUrl: './promptions-card.component.scss',
 })
-export class PromptionsCardComponent {}
+export class PromptionsCardComponent {
+  @Input() promotion!: CardPromotionModel;
+}
