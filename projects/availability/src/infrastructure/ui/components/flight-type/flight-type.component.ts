6cb655fbc0575f51b255542b09148f9fe9e1f46f
Bymarium <bymarium@gmail.com>
2025-03-12 11:22:06 -0500

    feat(flight-type): create component for flight type

---
+++ projects/availability/src/infrastructure/ui/components/flight-type/flight-type.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-flight-type',
+  imports: [],
+  templateUrl: './flight-type.component.html',
+  styleUrl: './flight-type.component.scss'
+})
+export class FlightTypeComponent {
+
+}
--- projects/availability/src/infrastructure/ui/containers/flight-selection/flight-selection.component.ts
+++ projects/availability/src/infrastructure/ui/containers/flight-selection/flight-selection.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FlightTypeComponent } from '../../components/flight-type/flight-type.component';
 
 @Component({
   selector: 'lib-flight-selection',
-  imports: [],
+  imports: [FlightTypeComponent],
   templateUrl: './flight-selection.component.html'
 })
 export class FlightSelectionComponent {
