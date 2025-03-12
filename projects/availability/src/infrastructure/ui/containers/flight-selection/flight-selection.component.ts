142dee453434964c47ae678541c9b514887c5773
Bymarium <bymarium@gmail.com>
2025-03-12 16:55:09 -0500

    feat(fligths): create available flight content component

--- projects/availability/src/infrastructure/ui/containers/flight-selection/flight-selection.component.ts
+++ projects/availability/src/infrastructure/ui/containers/flight-selection/flight-selection.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FlightTypeComponent } from '../../components/flight-type/flight-type.component';
+import { FlightsComponent } from '../../components/flights/flights.component';
 
 @Component({
   selector: 'lib-flight-selection',
-  imports: [FlightTypeComponent],
+  imports: [FlightTypeComponent, FlightsComponent],
   templateUrl: './flight-selection.component.html'
 })
 export class FlightSelectionComponent {
