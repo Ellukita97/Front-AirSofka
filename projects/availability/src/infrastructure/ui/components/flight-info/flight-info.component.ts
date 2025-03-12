452aa5de2a48e3517f05da0a1ebf2fd4d6e0338e
Bymarium <bymarium@gmail.com>
2025-03-12 17:22:41 -0500

    feat(flight-info): separate flight information into a new component

---
+++ projects/availability/src/infrastructure/ui/components/flight-info/flight-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-flight-info',
+  imports: [],
+  templateUrl: './flight-info.component.html',
+  styleUrl: './flight-info.component.scss'
+})
+export class FlightInfoComponent {
+
+}
--- projects/availability/src/infrastructure/ui/components/flights/flights.component.ts
+++ projects/availability/src/infrastructure/ui/components/flights/flights.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FlightInfoComponent } from '../flight-info/flight-info.component';
 
 @Component({
   selector: 'lib-flights',
-  imports: [],
+  imports: [FlightInfoComponent],
   templateUrl: './flights.component.html',
   styleUrl: './flights.component.scss'
 })
