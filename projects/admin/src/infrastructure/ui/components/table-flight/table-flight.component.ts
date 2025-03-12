56a4494a58502b1172ddf106611269013a902ff8
MartyPR <martin.p1131@gmail.com>
2025-03-11 21:45:35 -0500

    chore(feature/flight_admin):add table structure to show flights

---
+++ projects/admin/src/infrastructure/ui/components/table-flight/table-flight.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-table-flight',
+  imports: [],
+  templateUrl: './table-flight.component.html',
+  styleUrl: './table-flight.component.css'
+})
+export class TableFlightComponent {
+
+}
---
+++ projects/admin/src/infrastructure/ui/forms/create-flight/create-flight.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-create-flight',
+  imports: [],
+  templateUrl: './create-flight.component.html',
+  styleUrl: './create-flight.component.scss'
+})
+export class CreateFlightComponent {
+
+}
