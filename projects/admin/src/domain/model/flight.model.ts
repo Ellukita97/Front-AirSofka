309164d64290e3ee87ce398e53ef27a530b88f0b
MartyPR <martin.p1131@gmail.com>
2025-03-12 11:48:42 -0500

    feat(feature/flight_admin): add style to table flights

---
+++ projects/admin/src/domain/model/flight.model.ts
@@ -0,0 +1,52 @@
+export interface IFlight {
+    flightId: string;
+    flightNumber: string;
+    flightModel: string;
+    routeId: string;
+    departureTime: string; 
+    arrivalTime: string;
+    status: string;
+    seats: number;
+    tax: number;
+  
+    businessFullPrice: number;
+    taxBusinessFullPrice: number;
+    totalPriceBusinessFullPrice: number;
+  
+    businessBasicPrice: number;
+    taxBusinessBasicPrice: number;
+    totalPriceBusinessBasicPrice: number;
+  
+    economyFullPrice: number;
+    taxEconomyFullPrice: number;
+    totalPriceEconomyFullPrice: number;
+  
+    economyBasicPrice: number;
+    taxEconomyBasicPrice: number;
+    totalPriceEconomyBasicPrice: number;
+  
+    economyClassicPrice: number;
+    taxEconomyClassicPrice: number;
+    totalPriceEconomyClassicPrice: number;
+  }
+
+  export interface IFlightRequest {
+    flightNumber: string;
+    flightModel: string;
+    routeId: string;
+    price: number;
+    departureTime: string;
+    arrivalTime: string;
+  }
+
+   export function mapFlightToRequest(flight: IFlight): IFlightRequest {
+    return {
+      flightNumber: flight.flightNumber,
+      flightModel: flight.flightModel,
+      routeId: flight.routeId,
+      price: flight.economyBasicPrice, 
+      departureTime: flight.departureTime,
+      arrivalTime: flight.arrivalTime,
+    };
+  }
+  
