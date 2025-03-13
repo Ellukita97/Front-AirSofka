24b1ff80a1328225734600aa5acc8c3deea05524
CarlosCuadrado-24 <carloscuadradot24@gmail.com>
2025-03-13 11:21:40 -0500

    feat(register): form register realized

--- projects/authentication/src/infrastructure/ui/routes/guards/users.routes.ts
+++ projects/authentication/src/infrastructure/ui/routes/guards/users.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from "@angular/router";
 import { UsersContainerComponent } from "../../containers/user-container/user-container.component";
 import { SidebarComponent } from "shared";
+import { UserRegisterComponent } from "../../forms/user-register/user-register.component";
+import { RegisterContainerComponent } from "../../containers/register-container/register-container.component";
 
 export const authRoutes: Routes = [
     {
         path: '',
         children: [
             {
-            path: '',
-            component: UsersContainerComponent,
+                path: '',
+                component: UsersContainerComponent,
             },
             {
                 path: '',
@@ -16,5 +18,14 @@ export const authRoutes: Routes = [
                 outlet: 'header'
             }
         ],
+    },
+    {
+        path:'register',
+        children: [
+            {
+                path: '',
+                component:  RegisterContainerComponent
+            }
+        ]
     }
 ]
